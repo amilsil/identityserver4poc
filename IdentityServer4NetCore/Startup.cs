990c19c5163003c1878e47cc944f3f0b44dfd690
Amil <amilsil@gmail.com>
2017-03-28 22:13:29 +1100

    Use Postgres Db for IdentityServer

--- IdentityServer4NetCore/Startup.cs
+++ IdentityServer4NetCore/Startup.cs
@@ -1,50 +1,96 @@
-﻿using IdentityModel;
-using IdentityServer4.Models;
-using IdentityServer4.Stores;
+﻿using IdentityServer4.Models;
+using IdentityServer4.Postgresql.Entities;
+using IdentityServer4.Postgresql.Extensions;
+using IdentityServer4.Postgresql.Mappers;
+using Marten;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace IdentityServer4NetCore
 {
     class Startup
     {
+        private const string connection = "host=localhost;database=id-db;user id=postgres; Password=rocket,4181";
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
 
-            services.AddSingleton<IClientStore, CustomClientStore>();
+            //services.AddSingleton<IClientStore, CustomClientStore>();
             services.AddIdentityServer()
-                .AddTemporarySigningCredential()
-                .AddInMemoryApiResources(GetAllResources());
+                .AddConfigurationStore(connection)
+                .AddOperationalStore()
+                .AddTemporarySigningCredential();
+                //.AddTemporarySigningCredential()
+                //.AddInMemoryApiResources(GetAllResources());
 
         }
 
-        public IEnumerable<ApiResource> GetAllResources()
+        private void InitData(IApplicationBuilder app)
         {
-            return new[]
+            var store = DocumentStore.For(connection);
+            store.Advanced.Clean.CompletelyRemoveAll();
+            using (var session = store.LightweightSession())
             {
-                // Add a resource for some set of APIs that we may be protecting
-                // Note that the constructor will automatically create an allowed scope with
-                // name and claims equal to the resource's name and claims. If the resource
-                // has different scopes/levels of access, the scopes property can be set to
-                // list specific scopes included in this resource, instead.
-                new ApiResource(
-                    "myAPIs",                                       // Api resource name
-                    "My API Set #1",                                // Display name
-                    new[] { JwtClaimTypes.Name, JwtClaimTypes.Role, "office" }) // Claims to be included in access token
-                };
+                if (!session.Query<IdentityServer4.Postgresql.Entities.ApiResource>().Any())
+                {
+                    var resources = new List<IdentityServer4.Postgresql.Entities.ApiResource> {
+                     new IdentityServer4.Postgresql.Entities.ApiResource{ Name = "api1" , Description = "Api" , DisplayName ="api1" , Scopes = new List<ApiScope> { new ApiScope { Name = "api1" , DisplayName ="api1"  } } },
+
+                    };
+                    session.StoreObjects(resources);
+                }
+
+                if (!session.Query<IdentityServer4.Postgresql.Entities.IdentityResource>().Any())
+                {
+                    var resources = new List<IdentityServer4.Postgresql.Entities.IdentityResource> {
+                        new IdentityResources.OpenId().ToEntity(),
+                        new IdentityResources.Profile().ToEntity(),
+                        new IdentityResources.Email().ToEntity(),
+                        new IdentityResources.Phone().ToEntity()
+                    };
+                    session.StoreObjects(resources);
+                }
+                if (!session.Query<IdentityServer4.Postgresql.Entities.Client>().Any())
+                {
+                    var clients = new List<IdentityServer4.Postgresql.Entities.Client>
+                    {
+                          new IdentityServer4.Postgresql.Entities.Client
+                            {
+                                Id = "ro.client",
+                                ClientId ="ro.client",
+                                ClientName = "mvc",
+                                AllowedGrantTypes =  new List<ClientGrantType> { new ClientGrantType { GrantType = GrantType.Hybrid } , new ClientGrantType {  GrantType = GrantType.ClientCredentials} },
+                                AllowedCorsOrigins =  new List<ClientCorsOrigin>  {new ClientCorsOrigin { Origin = "http://localhost:5003" } },
+                                RequireClientSecret = true,
+                                ClientSecrets = new List<ClientSecret> { new ClientSecret { Value = "secret".Sha256() }  },
+                                RequireConsent = false,
+                                AllowedScopes = new List<ClientScope>{
+                                     new ClientScope { Scope = IdentityServer4.IdentityServerConstants.StandardScopes.OpenId },
+                                     new ClientScope { Scope = IdentityServer4.IdentityServerConstants.StandardScopes.Profile },
+                                     new ClientScope { Scope ="api1" }
+                                },
+                                RedirectUris = new List<ClientRedirectUri> { new ClientRedirectUri { RedirectUri ="http://localhost:5003/signin-oidc" } }
+                            }
+
+                        };
+                    session.StoreObjects(clients);
+                }
+                session.SaveChanges();
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            InitData(app);
+
             loggerFactory.AddConsole();
 
             if (env.IsDevelopment())
@@ -55,30 +101,6 @@ namespace IdentityServer4NetCore
             app.UseIdentityServer();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
-        }
-    }
-
-    public class CustomClientStore : IClientStore
-    {
-        public static IEnumerable<Client> AllClients { get; } = new[]
-        {
-        new Client
-        {
-            ClientId = "myClient",
-            ClientName = "My Custom Client",
-            AccessTokenLifetime = 60 * 60 * 24,
-            AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-            RequireClientSecret = false,
-            AllowedScopes =
-            {
-                "myAPIs"
-            }
-        }
-    };
-
-        public Task<Client> FindClientByIdAsync(string clientId)
-        {
-            return Task.FromResult(AllClients.FirstOrDefault(c => c.ClientId == clientId));
         }
     }
 }
